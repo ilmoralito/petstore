5b20b0de38f33e1a07532399c367fe550e9623e0
mario martinez <amakenadog@gmail.com>
2014-04-27 12:10:51 -0600

    Created testUser info

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,24 @@
+import ni.org.petstore.*
+import grails.util.GrailsUtil
+
 class BootStrap {
 
     def init = { servletContext ->
+    	def adminRole = new Role(authority: "ROLE_ADMIN").save()
+      def userRole = new Role(authority: "ROLE_USER").save()
+
+    	switch(GrailsUtil.environment) {
+    		case "development":
+    			def testUser = new User(username: "me", password: "password")
+    			testUser.save()
+
+					UserRole.create testUser, adminRole, true
+
+					assert User.count() == 1
+					assert Role.count() == 2
+					assert UserRole.count() == 1
+    		break
+    	}
     }
     def destroy = {
     }

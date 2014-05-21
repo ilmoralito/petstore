0b66966b0b3d90b40bf034bc4546a65ad4d766a8
Mario Martinez <amakenadog@gmail.com>
2014-05-21 15:46:56 -0600

    Implemented handlers in Presentation domain class

--- grails-app/domain/ni/org/petstore/Presentation.groovy
+++ grails-app/domain/ni/org/petstore/Presentation.groovy
@@ -1,12 +1,14 @@
 package ni.org.petstore
 
+import grails.util.Holders
+
 class Presentation implements Serializable {
 	String presentation
 	Integer quantity
 	BigDecimal price
 
   static constraints = {
-  	presentation inList:["Crema", "Spray", "Tableta", "Soluble", "Suspencion", "Inyectable"], maxSize:255
+  	presentation inList:Holders.config.ni.org.petstore.presentations, maxSize:255
   	quantity min:0
   	price min:1.0
   }
